0e4aeb389d648e5b5279cde785bdf66ae45ffd13
Hector <hector.mercadier@gmail.com>
2023-01-09 10:50:33 +0100

    working sunburst

--- sunburst_echarts.js
+++ sunburst_echarts.js
@@ -1,4 +1,3 @@
-import * as echarts from 'echarts';
 
 var app = {};
 
@@ -804,22 +803,10 @@ var data = [
   }
 ];
 option = {
-  title: {
-    text: 'WORLD COFFEE RESEARCH SENSORY LEXICON',
-    subtext: 'Source: https://worldcoffeeresearch.org/work/sensory-lexicon/',
-    textStyle: {
-      fontSize: 14,
-      align: 'center'
-    },
-    subtextStyle: {
-      align: 'center'
-    },
-    sublink: 'https://worldcoffeeresearch.org/work/sensory-lexicon/'
-  },
   series: {
     type: 'sunburst',
     data: data,
-    radius: [0, '95%'],
+    radius: [0, '100%'],
     sort: undefined,
     emphasis: {
       focus: 'ancestor'
@@ -832,27 +819,21 @@ option = {
         itemStyle: {
           borderWidth: 2
         },
-        label: {
-          rotate: 'tangential'
-        }
       },
       {
         r0: '35%',
         r: '70%',
-        label: {
-          align: 'right'
-        }
       },
       {
         r0: '70%',
-        r: '72%',
+        r: '77%',
         label: {
           position: 'outside',
           padding: 3,
           silent: false
         },
         itemStyle: {
-          borderWidth: 3
+          borderWidth: 2
         }
       }
     ]
